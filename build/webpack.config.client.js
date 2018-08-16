4c2a42b02c8b3ed93bd5f11a01660ebeace79e11
kovlento <bluskytee@163.com>
2018-08-16 23:44:19 +0800

    webpack-dev-server配置

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,14 +1,17 @@
 const path = require('path')
 const HTMLPlugin = require('html-webpack-plugin')
 
-module.exports = {
+// 	是否是开发环境
+const isDev = process.env.NODE_DEV === 'development'
+
+const config = {
 	entry: {
 		app: path.join(__dirname, '../client/app.js')
 	},
 	output: {
 		filename: '[name].[hash].js', //js文件改变，hash值改变，最大限度使用缓存
 		path: path.join(__dirname, '../dist'),
-		publicPath: './public' //区分是静态资源还是api等
+		publicPath: '/public' //区分是静态资源还是api等
 	},
 	module: {
 		rules: [{
@@ -28,3 +31,21 @@ module.exports = {
 		})
 	]
 }
+
+if (isDev) {
+	config.devServer = {
+		host: '0.0.0.0', //指向任何地址
+		port: '8888', //端口
+		contentBase: path.join(__dirname, '../dist'), // 编译的静态文件地址
+		// hot: true, // 启动hot module replacement
+		overlay: {
+			errors: true // 编译过程中的错误显示在页面中
+		},
+		publicPath: '/public/',
+		historyApiFallback: {
+			index: '/public/index.html',
+		}
+	}
+}
+
+module.exports = config
--- build/webpack.config.server.js
+++ build/webpack.config.server.js
@@ -8,7 +8,7 @@ module.exports = {
 	output: {
 		filename: 'server-entry.js', //js文件改变，hash值改变，最大限度使用缓存
 		path: path.join(__dirname, '../dist'),
-		publicPath: './public', //区分是静态资源还是api等
+		publicPath: '/public', //区分是静态资源还是api等
 		libraryTarget: 'commonjs2'
 	},
 	module: {
