2572fa98b59288527bd74d2e70203941278d86ea
kovlento <bluskytee@163.com>
2018-08-21 01:17:39 +0800

    add editorconfig

--- client/App.jsx
+++ client/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-export default class App extends React.Component {
-	render() {
-		return(
-			<div>This is app 123111212</div>
-		)
-	}
-}
+export default () => <div>This is app 123111212</div>
+
+// export default class App extends React.Component {
+//  render() {
+//    return(
+//      <div>This is app 123111212</div>
+//    )
+//  }
+// }
--- client/app.js
+++ client/app.js
@@ -1,31 +1,29 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import {
-	AppContainer
-} from 'react-hot-loader'
+import { AppContainer } from 'react-hot-loader' // eslint-disable-line
 import App from './App.jsx'
 
 // ReactDom.hydrate(<App />, document.getElementById('root'))
 
 const root = document.getElementById('root')
-const render = Component => {
-	ReactDom.hydrate(
-		<AppContainer>
-			<Component />
-		</AppContainer>,
-		root
-	)
+const render = (Component) => {
+  ReactDom.hydrate(
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    root,
+  )
 }
 
 render(App)
 
 if (module.hot) {
-	//当热更新的代码存在，把App.jsx重新加载一遍
-	module.hot.accept('./App.jsx', () => {
-		const NextApp = require('./App.jsx').default
-		//.default是因为在App里面export default这种写法。
+  // 当热更新的代码存在，把App.jsx重新加载一遍
+  module.hot.accept('./App.jsx', () => {
+    const NextApp = require('./App.jsx').default // eslint-disable-line
+    // .default是因为在App里面export default这种写法。
 
-		// ReactDom.hydrate(<NextApp />, document.getElementById('root'))
-		render(NextApp)
-	})
+    // ReactDom.hydrate(<NextApp />, document.getElementById('root'))
+    render(NextApp)
+  })
 }
