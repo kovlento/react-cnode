4a448edc3a48d4e750ab64e9868bd758ac14aa5a
kovlento <bluskytee@163.com>
2018-08-25 18:33:17 +0800

    add store

--- client/app.js
+++ client/app.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'mobx-react'
 import { AppContainer } from 'react-hot-loader' // eslint-disable-line
 import App from './views/App'
 
+import appState from './store/app-state'
 // ReactDom.hydrate(<App />, document.getElementById('root'))
 
 const root = document.getElementById('root')
 const render = (Component) => {
-  ReactDom.hydrate(
+  const renderMethod = module.hot ? ReactDom.render : ReactDom.hydrate
+  renderMethod(
     <AppContainer>
-      <BrowserRouter>
-        <Component />
-      </BrowserRouter>
+      <Provider appState={appState}>
+        <BrowserRouter>
+          <Component />
+        </BrowserRouter>
+      </Provider>
     </AppContainer>,
     root,
   )
