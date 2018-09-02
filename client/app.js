0ef16e6af10daf2c2b9a1ae13ccb87b1e52e5585
kovlento <bluskytee@163.com>
2018-09-02 20:59:54 +0800

    install material-ui

--- client/app.js
+++ client/app.js
@@ -3,13 +3,40 @@ import ReactDom from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'mobx-react'
 import { AppContainer } from 'react-hot-loader' // eslint-disable-line
-import App from './views/App'
 
+import { MuiThemeProvider, createMuiTheme } from 'material-ui'
+import { lightBlue, pink } from 'material-ui/colors'
+
+import App from './views/App'
 import AppState from './store/app-state'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: pink,
+    accent: lightBlue,
+    type: 'light',
+  },
+})
 // ReactDom.hydrate(<App />, document.getElementById('root'))
 
 // 读取initialState
 const initialState = window.__INITIAL__STATE__ || {}  // eslint-disable-line
+
+const createApp = (TheApp) => {
+  class Main extends React.Component {
+    // Remove the server-side injected CSS.
+    componentDidMount() {
+      const jssStyles = document.getElementById('jss-server-side');
+      if (jssStyles && jssStyles.parentNode) {
+        jssStyles.parentNode.removeChild(jssStyles);
+      }
+    }
+    render() {
+      return <TheApp />
+    }
+  }
+  return Main
+}
 
 const root = document.getElementById('root')
 const render = (Component) => {
@@ -18,7 +45,9 @@ const render = (Component) => {
     <AppContainer>
       <Provider appState={new AppState(initialState.appState)}>
         <BrowserRouter>
-          <Component />
+          <MuiThemeProvider theme={theme}>
+            <Component />
+          </MuiThemeProvider>
         </BrowserRouter>
       </Provider>
     </AppContainer>,
@@ -26,7 +55,7 @@ const render = (Component) => {
   )
 }
 
-render(App)
+render(createApp(App))
 
 if (module.hot) {
   // 当热更新的代码存在，把App.jsx重新加载一遍
@@ -35,6 +64,6 @@ if (module.hot) {
     // .default是因为在App里面export default这种写法。
 
     // ReactDom.hydrate(<NextApp />, document.getElementById('root'))
-    render(NextApp)
+    render(createApp(NextApp))
   })
 }
