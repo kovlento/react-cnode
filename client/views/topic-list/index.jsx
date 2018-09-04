922aa77ac393cbb345796e2e3f61b2ac5eb385da
kovlento <bluskytee@163.com>
2018-09-04 23:40:15 +0800

    主页样式

--- client/views/topic-list/index.jsx
+++ client/views/topic-list/index.jsx
@@ -3,16 +3,21 @@ import { observer, inject } from 'mobx-react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
+import Tabs, { Tab } from 'material-ui/Tabs'
 import Button from 'material-ui/Button'
 import { AppState } from '../../store/app-state'
 import Container from '../layout/container'
-
+import TopicListItem from './list-item'
 
 @inject('appState') @observer
 export default class TopicList extends React.Component {
   constructor() {
     super();
-    this.changeName = this.changeName.bind(this)
+    this.state = {
+      tabIndex: 0,
+    }
+    this.changeTab = this.changeTab.bind(this)
+    this.listItemClick = this.listItemClick.bind(this)
   }
 
   componentDidMount() {
@@ -28,20 +33,47 @@ export default class TopicList extends React.Component {
     })
   }
 
-  changeName(event) {
-    this.props.appState.changeName(event.target.value)
+  changeTab(e, index) {
+    this.setState({
+      tabIndex: index,
+    })
   }
 
+  /** eslint-disable */
+  listItemClick() {
+
+  }
+  /** eslint-enable */
+
   render() {
+    const {
+      tabIndex,
+    } = this.state
+
+    const topic = {
+      title: 'this is a title',
+      username: 'kovlento',
+      reply_count: 20,
+      visit_count: 100,
+      create_at: '2018-8-8',
+      tab: '置顶',
+    }
+
     return (
       <Container>
         <Helmet>
           <title>This is topic list</title>
           <meta name="description" content="This is description" />
         </Helmet>
-        <Button raised="true" color="primary">this is a button</Button>
-        <input type="text" onChange={this.changeName} />
-        <span>{this.props.appState.msg}</span>
+        <Tabs value={tabIndex} onChange={this.changeTab} >
+          <Tab label="全部" />
+          <Tab label="分享" />
+          <Tab label="工作" />
+          <Tab label="问答" />
+          <Tab label="精品" />
+          <Tab label="测试" />
+        </Tabs>
+        <TopicListItem onClick={this.listItemClick} topic={topic} />
       </Container>
     )
   }
